e0831431b7a05ec2c74c17f9e0e93b840eab39b2
Alexandre <alexandre@indev.net.br>
2020-08-03 18:37:54 -0300

    feature: update mixin to TS class

--- template/mixins/db.mixin.ts
+++ template/mixins/db.mixin.ts
@@ -5,12 +5,40 @@ import { sync } from "mkdirp";
 import { Context, Service, ServiceSchema } from "moleculer";
 import DbService from "moleculer-db";
 
-export default function(collection: string): Partial<ServiceSchema> & ThisType<Service> {
-	const cacheCleanEventName = `cache.clean.${collection}`;
+class Connection extends Partial<ServiceSchema>, ThisType<Service>{
+	private cacheCleanEventName: string;
+	private collection:string;
 
-	const schema: Partial<ServiceSchema> & ThisType<Service> = {
+	constructor(collection: string) {
+		this.collection = collection;
+		this.cacheCleanEventName = `cache.clean.${collection}`;
+	}
+	start(){
+		if (process.env.MONGO_URI) {
+			// Mongo adapter
+			const MongoAdapter = require("moleculer-db-adapter-mongo");
+
+			this.schema.adapter = new MongoAdapter(process.env.MONGO_URI);
+			this.schema.collection = this.collection;
+		} else if (process.env.TEST) {
+			// NeDB memory adapter for testing
+			// @ts-ignore
+			this.schema.adapter = new DbService.MemoryAdapter();
+		} else {
+			// NeDB file DB adapter
+
+			// Create data folder
+			if (!existsSync("./data")) {
+				sync("./data");
+			}
+			// @ts-ignore
+			this.schema.adapter = new DbService.MemoryAdapter({ filename: `./data/${this.collection}.db` });
+		}
+
+		return this.schema;
+	}
+	schema: Partial<ServiceSchema> & ThisType<Service> = {
 		mixins: [DbService],
-
 		events: {
 			/**
 			 * Subscribe to the cache clean event. If it's triggered
@@ -18,13 +46,12 @@ export default function(collection: string): Partial<ServiceSchema> & ThisType<S
 			 *
 			 * @param {Context} ctx
 			 */
-			async [cacheCleanEventName]() {
+			async [this.cacheCleanEventName]() {
 				if (this.broker.cacher) {
 					await this.broker.cacher.clean(`${this.fullName}.*`);
 				}
 			},
 		},
-
 		methods: {
 			/**
 			 * Send a cache clearing event when an entity changed.
@@ -34,44 +61,22 @@ export default function(collection: string): Partial<ServiceSchema> & ThisType<S
 			 * @param {Context} ctx
 			 */
 			async entityChanged(type: string, json: any, ctx: Context) {
-				ctx.broadcast(cacheCleanEventName);
+				ctx.broadcast(this.cacheCleanEventName);
 			},
 		},
-
 		async started() {
 			// Check the count of items in the DB. If it's empty,
 			// call the `seedDB` method of the service.
 			if (this.seedDB) {
 				const count = await this.adapter.count();
 				if (count === 0) {
-					this.logger.info(`The '${collection}' collection is empty. Seeding the collection...`);
+					this.logger.info(`The '${this.collection}' collection is empty. Seeding the collection...`);
 					await this.seedDB();
 					this.logger.info("Seeding is done. Number of records:", await this.adapter.count());
 				}
 			}
 		},
-	};
-
-	if (process.env.MONGO_URI) {
-		// Mongo adapter
-		const MongoAdapter = require("moleculer-db-adapter-mongo");
-
-		schema.adapter = new MongoAdapter(process.env.MONGO_URI);
-		schema.collection = collection;
-	} else if (process.env.TEST) {
-		// NeDB memory adapter for testing
-		// @ts-ignore
-		schema.adapter = new DbService.MemoryAdapter();
-	} else {
-		// NeDB file DB adapter
-
-		// Create data folder
-		if (!existsSync("./data")) {
-			sync("./data");
-		}
-		// @ts-ignore
-		schema.adapter = new DbService.MemoryAdapter({ filename: `./data/${collection}.db` });
 	}
 
-	return schema;
 }
+export default Conection
