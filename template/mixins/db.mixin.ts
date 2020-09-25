b84ff130bed8071cdde51495079bdb24279aa329
Oanh Nguyen <oanhnn.bk@gmail.com>
2020-09-25 10:49:58 +0700

    Fix coding style and unit test

--- template/mixins/db.mixin.ts
+++ template/mixins/db.mixin.ts
@@ -9,73 +9,74 @@ class Connection implements Partial<ServiceSchema>, ThisType<Service>{
 
 	private cacheCleanEventName: string;
 	private collection: string;
-	private schema: Partial<ServiceSchema> & ThisType<Service> ;
+	private schema: Partial<ServiceSchema> & ThisType<Service>;
 
 	public constructor(public collectionName: string) {
 		this.collection = collectionName;
 		this.cacheCleanEventName = `cache.clean.${this.collection}`;
 		this.schema = {
-		mixins: [DbService],
-		events: {
-			/**
-			 * Subscribe to the cache clean event. If it's triggered
-			 * clean the cache entries for this service.
-			 *
-			 */
-			async [this.cacheCleanEventName]() {
-				if (this.broker.cacher) {
-					await this.broker.cacher.clean(`${this.fullName}.*`);
+			mixins: [DbService],
+			events: {
+				/**
+				 * Subscribe to the cache clean event. If it's triggered
+				 * clean the cache entries for this service.
+				 *
+				 */
+				async [this.cacheCleanEventName]() {
+					if (this.broker.cacher) {
+						await this.broker.cacher.clean(`${this.fullName}.*`);
+					}
+				},
+			},
+			methods: {
+				/**
+				 * Send a cache clearing event when an entity changed.
+				 *
+				 * @param {String} type
+				 * @param {any} json
+				 * @param {Context} ctx
+				 */
+				entityChanged: async (type: string, json: any, ctx: Context) => {
+					await  ctx.broadcast(this.cacheCleanEventName);
+				},
+			},
+			async started() {
+				// Check the count of items in the DB. If it's empty,
+				// Call the `seedDB` method of the service.
+				if (this.seedDB) {
+					const count = await this.adapter.count();
+					if (count === 0) {
+						this.logger.info(`The '${this.collection}' collection is empty. Seeding the collection...`);
+						await this.seedDB();
+						this.logger.info("Seeding is done. Number of records:", await this.adapter.count());
+					}
 				}
 			},
-		},
-		methods: {
-			/**
-			 * Send a cache clearing event when an entity changed.
-			 *
-			 * @param {String} type
-			 * @param {any} json
-			 * @param {Context} ctx
-			 */
-			async entityChanged(type: string, json: any, ctx: Context) {
-				await  ctx.broadcast(this.cacheCleanEventName);
-			},
-		},
-		async started() {
-			// Check the count of items in the DB. If it's empty,
-			// Call the `seedDB` method of the service.
-			if (this.seedDB) {
-				const count = await this.adapter.count();
-				if (count === 0) {
-					this.logger.info(`The '${this.collection}' collection is empty. Seeding the collection...`);
-					await this.seedDB();
-					this.logger.info("Seeding is done. Number of records:", await this.adapter.count());
-				}
-			}
-		},
-	};
+		};
 	}
+
 	public start(){
-			if (process.env.MONGO_URI) {
-				// Mongo adapter
-				// eslint-disable-next-line @typescript-eslint/no-var-requires
-				const   MongoAdapter = require("moleculer-db-adapter-mongo");
+		if (process.env.MONGO_URI) {
+			// Mongo adapter
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			const   MongoAdapter = require("moleculer-db-adapter-mongo");
 
-				this.schema.adapter = new MongoAdapter(process.env.MONGO_URI);
-				this.schema.collection = this.collection;
-			} else if (process.env.TEST) {
-				// NeDB memory adapter for testing
-				// @ts-ignore
-				this.schema.adapter = new DbService.MemoryAdapter();
-			} else {
-				// NeDB file DB adapter
+			this.schema.adapter = new MongoAdapter(process.env.MONGO_URI);
+			this.schema.collection = this.collection;
+		} else if (process.env.TEST) {
+			// NeDB memory adapter for testing
+			// @ts-ignore
+			this.schema.adapter = new DbService.MemoryAdapter();
+		} else {
+			// NeDB file DB adapter
 
-				// Create data folder
-				if (!existsSync("./data")) {
-					sync("./data");
-				}
-				// @ts-ignore
-				this.schema.adapter = new DbService.MemoryAdapter({ filename: `./data/${this.collection}.db` });
+			// Create data folder
+			if (!existsSync("./data")) {
+				sync("./data");
 			}
+			// @ts-ignore
+			this.schema.adapter = new DbService.MemoryAdapter({ filename: `./data/${this.collection}.db` });
+		}
 
 		return this.schema;
 	}
--- template/services/products.service.ts
+++ template/services/products.service.ts
@@ -4,113 +4,113 @@ import {Context, Service, ServiceBroker} from "moleculer";
 import DbConnection from "../mixins/db.mixin";
 
 export default class ProductsService extends Service{
-    private DbMixin = new DbConnection("products").start();
+	private DbMixin = new DbConnection("products").start();
 	public  constructor(public broker: ServiceBroker) {
-        super(broker);
-        this.parseServiceSchema({
-            name: "products",
-            mixins: [this.DbMixin],
-            settings: {
-                // Available fields in the responses
-                fields: [
-                    "_id",
-                    "name",
-                    "quantity",
-                    "price",
-                ],
+		super(broker);
+		this.parseServiceSchema({
+			name: "products",
+			mixins: [this.DbMixin],
+			settings: {
+				// Available fields in the responses
+				fields: [
+					"_id",
+					"name",
+					"quantity",
+					"price",
+				],
 
-                // Validator for the `create` & `insert` actions.
-                entityValidator: {
-                    name: "string|min:3",
-                    price: "number|positive",
-                },
-            },
-            hooks: {
-                before: {
-                    /**
-                     * Register a before hook for the `create` action.
-                     * It sets a default value for the quantity field.
-                     *
-                     * @param {Context} ctx
-                     */
-                    create: (ctx: Context<{ quantity: number }>) => {
-                        ctx.params.quantity = 0;
-                    },
-                },
-            },
-            actions: {
-                /**
-                 * The "moleculer-db" mixin registers the following actions:
-                 *  - list
-                 *  - find
-                 *  - count
-                 *  - create
-                 *  - insert
-                 *  - update
-                 *  - remove
-                 */
+				// Validator for the `create` & `insert` actions.
+				entityValidator: {
+					name: "string|min:3",
+					price: "number|positive",
+				},
+			},
+			hooks: {
+				before: {
+					/**
+					 * Register a before hook for the `create` action.
+					 * It sets a default value for the quantity field.
+					 *
+					 * @param {Context} ctx
+					 */
+					create: (ctx: Context<{ quantity: number }>) => {
+						ctx.params.quantity = 0;
+					},
+				},
+			},
+			actions: {
+				/**
+				 * The "moleculer-db" mixin registers the following actions:
+				 *  - list
+				 *  - find
+				 *  - count
+				 *  - create
+				 *  - insert
+				 *  - update
+				 *  - remove
+				 */
 
-                // --- ADDITIONAL ACTIONS ---
+				// --- ADDITIONAL ACTIONS ---
 
-                /**
-                 * Increase the quantity of the product item.
-                 */
-                increaseQuantity: {
-                    rest: "PUT /:id/quantity/increase",
-                    params: {
-                        id: "string",
-                        // @ts-ignore
-                        value: "number|integer|positive",
-                    },
-                    async handler(ctx: Context<{ id: string; value: number }>) {
-                        const doc = await this.adapter.updateById(ctx.params.id, { $inc: { quantity: ctx.params.value } });
-                        const json = await this.transformDocuments(ctx, ctx.params, doc);
-                        await this.entityChanged("updated", json, ctx);
+				/**
+				 * Increase the quantity of the product item.
+				 */
+				increaseQuantity: {
+					rest: "PUT /:id/quantity/increase",
+					params: {
+						id: "string",
+						// @ts-ignore
+						value: "number|integer|positive",
+					},
+					async handler(ctx: Context<{ id: string; value: number }>) {
+						const doc = await this.adapter.updateById(ctx.params.id, { $inc: { quantity: ctx.params.value } });
+						const json = await this.transformDocuments(ctx, ctx.params, doc);
+						await this.entityChanged("updated", json, ctx);
 
-                        return json;
-                    },
-                },
+						return json;
+					},
+				},
 
-                /**
-                 * Decrease the quantity of the product item.
-                 */
-                decreaseQuantity: {
-                    rest: "PUT /:id/quantity/decrease",
-                    params: {
-                        id: "string",
-                        // @ts-ignore
-                        value: "number|integer|positive",
-                    },
-                    /** @param {Context} ctx  */
-                    async handler(ctx: Context<{ id: string; value: number }>) {
-                        const doc = await this.adapter.updateById(ctx.params.id, { $inc: { quantity: -ctx.params.value } });
-                        const json = await this.transformDocuments(ctx, ctx.params, doc);
-                        await this.entityChanged("updated", json, ctx);
+				/**
+				 * Decrease the quantity of the product item.
+				 */
+				decreaseQuantity: {
+					rest: "PUT /:id/quantity/decrease",
+					params: {
+						id: "string",
+						// @ts-ignore
+						value: "number|integer|positive",
+					},
+					/** @param {Context} ctx  */
+					async handler(ctx: Context<{ id: string; value: number }>) {
+						const doc = await this.adapter.updateById(ctx.params.id, { $inc: { quantity: -ctx.params.value } });
+						const json = await this.transformDocuments(ctx, ctx.params, doc);
+						await this.entityChanged("updated", json, ctx);
 
-                        return json;
-                    },
-                },
-            },
-            methods: {
-                /**
-                 * Loading sample data to the collection.
-                 * It is called in the DB.mixin after the database
-                 * connection establishing & the collection is empty.
-                 */
-                async seedDB() {
-                    await this.adapter.insertMany([
-                        { name: "Samsung Galaxy S10 Plus", quantity: 10, price: 704 },
-                        { name: "iPhone 11 Pro", quantity: 25, price: 999 },
-                        { name: "Huawei P30 Pro", quantity: 15, price: 679 },
-                    ]);
-                },
-            },
+						return json;
+					},
+				},
+			},
+			methods: {
+				/**
+				 * Loading sample data to the collection.
+				 * It is called in the DB.mixin after the database
+				 * connection establishing & the collection is empty.
+				 */
+				async seedDB() {
+					await this.adapter.insertMany([
+						{ name: "Samsung Galaxy S10 Plus", quantity: 10, price: 704 },
+						{ name: "iPhone 11 Pro", quantity: 25, price: 999 },
+						{ name: "Huawei P30 Pro", quantity: 15, price: 679 },
+					]);
+				},
+			},
 			/**
 			 * Loading sample data to the collection.
-            async afterConnected() {
-             await this.adapter.collection.createIndex({ name: 1 });
-            },
+			async afterConnected() {
+			 await this.adapter.collection.createIndex({ name: 1 });
+			},
 			 */
-        });
-    }
+		});
+	}
 }
