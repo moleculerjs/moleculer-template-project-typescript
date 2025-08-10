ddd894f1cdbb89904b57498bd9a18a32ea1e5580
Icebob <mereg.norbert@gmail.com>
2025-08-10 22:18:58 +0200

    remove not used type

--- template/types/moleculer.d.ts
+++ template/types/moleculer.d.ts
@@ -4,17 +4,4 @@ declare module "moleculer" {
 	interface ActionSchema {
 		auth?: "required" | null | undefined;
 	}
-
-	interface ServiceBroker {
-		sendToChannel(channelName: string, payload: unknown, opts?: { ctx?: Context });
-	}
-
-	interface ServiceSchema {
-		channels?: {
-			[key: string]: {
-				context?: boolean;
-				handler: (this: Service, ctx: Context<P, M, L, H>) => Promise<void>;
-			};
-		};
-	}
 }
